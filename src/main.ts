ddb84eda5eb463492c6399fb7958234d07d3e039
Felipe Custodio <felipecustodio@proton.me>
2025-12-05 16:55:04 -0300

    small UI refactor while also implementing grid resize handling

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { Game, GameStatesEnum } from './game';
 import { GameEventsEnum } from './gameEventsManager';
 import { Coordinates } from './grid';
 
+const GRID_MARGIN_X = 3;
+const GRID_MARGIN_Y = 1;
+const CELL_SIZE = 20;
+
 const canvasContainer =
   document.querySelector<HTMLDivElement>('#canvasContainer');
 if (!canvasContainer) throw Error();
@@ -28,59 +32,95 @@ if (
 )
   throw Error();
 
-const canvasClientRect = canvasContainer.getBoundingClientRect();
+function debounce<T extends (...args: unknown[]) => void>(
+  wait: number,
+  callback: T
+) {
+  let timeout: ReturnType<typeof setTimeout> | null;
 
-const GRID_MARGIN_X = 3;
-const GRID_MARGIN_Y = 1;
-const CELL_SIZE = 20;
+  return function <U>(this: U, ...args: Parameters<typeof callback>) {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const context = this;
 
-const canvasWidth =
-  Math.floor(canvasClientRect.width / CELL_SIZE) * CELL_SIZE -
-  CELL_SIZE * GRID_MARGIN_X;
-const canvasHeight =
-  Math.floor(canvasClientRect.height / CELL_SIZE) * CELL_SIZE -
-  CELL_SIZE * GRID_MARGIN_Y;
+    if (typeof timeout === 'number') {
+      clearTimeout(timeout);
+    }
 
-bgCanvas.width = canvasWidth;
-bgCanvas.height = canvasHeight;
-gameCanvas.width = canvasWidth;
-gameCanvas.height = canvasHeight;
+    timeout = setTimeout(() => {
+      timeout = null;
+      callback.apply(context, args);
+    }, wait);
 
-const canvasMarginInline = (canvasClientRect.width - canvasWidth) / 2;
-const canvasMarginBlock = (canvasClientRect.height - canvasHeight) / 2;
+    if (!timeout) {
+      callback.apply(context, args);
+    }
+  };
+}
 
-bgCanvas.style.marginInline = canvasMarginInline + 'px';
-bgCanvas.style.marginBlock = canvasMarginBlock + 'px';
-gameCanvas.style.marginInline = canvasMarginInline + 'px';
-gameCanvas.style.marginBlock = canvasMarginBlock + 'px';
+function setupCanvases(
+  canvasContainer: HTMLDivElement,
+  bgCanvas: HTMLCanvasElement,
+  gameCanvas: HTMLCanvasElement
+) {
+  const canvasClientRect = canvasContainer.getBoundingClientRect();
+
+  const canvasWidth =
+    Math.floor(canvasClientRect.width / CELL_SIZE) * CELL_SIZE -
+    CELL_SIZE * GRID_MARGIN_X;
+  const canvasHeight =
+    Math.floor(canvasClientRect.height / CELL_SIZE) * CELL_SIZE -
+    CELL_SIZE * GRID_MARGIN_Y;
+
+  bgCanvas.width = canvasWidth;
+  bgCanvas.height = canvasHeight;
+  gameCanvas.width = canvasWidth;
+  gameCanvas.height = canvasHeight;
+
+  const canvasMarginInline = (canvasClientRect.width - canvasWidth) / 2;
+  const canvasMarginBlock = (canvasClientRect.height - canvasHeight) / 2;
+
+  bgCanvas.style.marginInline = canvasMarginInline + 'px';
+  bgCanvas.style.marginBlock = canvasMarginBlock + 'px';
+  gameCanvas.style.marginInline = canvasMarginInline + 'px';
+  gameCanvas.style.marginBlock = canvasMarginBlock + 'px';
+}
+
+setupCanvases(canvasContainer, bgCanvas, gameCanvas);
 
 const bgCtx = bgCanvas.getContext('2d');
 const gameCtx = gameCanvas.getContext('2d');
 
 if (!bgCtx || !gameCtx) throw Error();
 
-const gridDimensions = {
-  height: Math.floor(bgCanvas.clientHeight / CELL_SIZE),
-  width: Math.floor(bgCanvas.clientWidth / CELL_SIZE),
-};
+function calculateGridDimensions(bgCanvas: HTMLCanvasElement) {
+  return {
+    height: Math.floor(bgCanvas.clientHeight / CELL_SIZE),
+    width: Math.floor(bgCanvas.clientWidth / CELL_SIZE),
+  };
+}
 
 const game = new Game({
-  grid: gridDimensions,
+  grid: calculateGridDimensions(bgCanvas),
   events: true,
   delay: 10 - parseInt(speedSlider.value),
 });
 
-for (let y = 0; y < gridDimensions.height; y++) {
-  for (let x = 0; x < gridDimensions.width; x++) {
-    bgCtx.strokeStyle = '#dee2e610';
-    bgCtx.strokeRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+function drawBackgroundCanvas(
+  bgCanvas: HTMLCanvasElement,
+  bgCtx: CanvasRenderingContext2D
+) {
+  for (let y = 0; y < calculateGridDimensions(bgCanvas).height; y++) {
+    for (let x = 0; x < calculateGridDimensions(bgCanvas).width; x++) {
+      bgCtx.strokeStyle = '#dee2e610';
+      bgCtx.strokeRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    }
   }
 }
 
+drawBackgroundCanvas(bgCanvas, bgCtx);
+
 gameCanvas.addEventListener('click', (event) => {
   const rect = gameCanvas.getBoundingClientRect();
-
-  console.log(rect);
 
   const y = Math.floor((event.clientY - rect.top) / CELL_SIZE);
   const x = Math.floor((event.clientX - rect.left) / CELL_SIZE);
@@ -91,9 +131,8 @@ gameCanvas.addEventListener('click', (event) => {
   });
 });
 
-window.addEventListener('resize', (event) => {
-  console.log('resize');
-  console.log(event);
+window.addEventListener('resize', () => {
+  game.eventManager?.emitGridResized();
 });
 
 btnToggleRun.addEventListener('click', (event) => {
@@ -107,16 +146,17 @@ btnClear.addEventListener('click', () => {
   game.reset();
 });
 
-speedSlider.addEventListener('change', () => {
-  game.changeDelay(10 - parseInt(speedSlider.value));
-});
+speedSlider.addEventListener(
+  'change',
+  debounce(300, () => game.changeDelay(10 - parseInt(speedSlider.value)))
+);
 
 btnNext.addEventListener('click', () => game.nextGrid());
 btnPrev.addEventListener('click', () => game.previousGrid());
 
 game.eventManager?.on(GameEventsEnum.GRID_RESETED, () => {
-  for (let y = 0; y < gridDimensions.height; y++) {
-    for (let x = 0; x < gridDimensions.width; x++) {
+  for (let y = 0; y < calculateGridDimensions(bgCanvas).height; y++) {
+    for (let x = 0; x < calculateGridDimensions(bgCanvas).width; x++) {
       gameCtx.clearRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
     }
   }
@@ -158,9 +198,9 @@ game.eventManager?.on(GameEventsEnum.GAME_STATE_CHANGED, (event) => {
       btnToggleRun.setAttribute('disabled', 'true');
       btnToggleRun.textContent = 'Iniciar';
       speedSlider.setAttribute('disabled', 'true');
-      btnClear.removeAttribute('disabled');
       btnNext.setAttribute('disabled', 'true');
       btnPrev.removeAttribute('disabled');
+      if (game.getPopulation() > 0) btnClear.removeAttribute('disabled');
       break;
   }
 });
@@ -179,9 +219,39 @@ game.eventManager?.on(GameEventsEnum.GRID_UPDATED, () => {
     if (game.getHistoryLength() < 1) btnPrev.setAttribute('disabled', 'true');
     else btnPrev.removeAttribute('disabled');
   }
-  population.textContent = game.getPopulation().toString();
+  const populationCount = game.getPopulation();
+  population.textContent = populationCount.toString();
   generation.textContent = (game.getHistoryLength() + 1).toString();
 });
+
+game.eventManager?.on(
+  GameEventsEnum.GRID_RESIZED,
+  debounce(100, () => {
+    handleGridResize(canvasContainer, bgCanvas, gameCanvas, bgCtx, gameCtx);
+  })
+);
+
+function handleGridResize(
+  canvasContainer: HTMLDivElement,
+  bgCanvas: HTMLCanvasElement,
+  gameCanvas: HTMLCanvasElement,
+  bgCtx: CanvasRenderingContext2D,
+  gameCtx: CanvasRenderingContext2D
+) {
+  setupCanvases(canvasContainer, bgCanvas, gameCanvas);
+  drawBackgroundCanvas(bgCanvas, bgCtx);
+  const { height, width } = calculateGridDimensions(bgCanvas);
+  game.changeGridDimensions({ height, width });
+  const grid = game.getGrid();
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      if (grid.getAtWithMetadata({ y, x }).value) {
+        gameCtx.fillStyle = '#0d6efd';
+        gameCtx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      }
+    }
+  }
+}
 
 game.eventManager?.on(
   GameEventsEnum.CELL_BORN,
